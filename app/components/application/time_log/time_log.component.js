456c821e1e3872b55290d8959d38c297647c103e
Dmitry Goncharuk <goncharuk4dmitry@gmail.com>
2017-02-25 12:19:27 +0300

    Implement time counter for TimeLog components.

--- app/components/application/time_log/time_log.component.js
+++ app/components/application/time_log/time_log.component.js
@@ -1,11 +1,40 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {getSpentTime} from '../../../lib/time_log_helpers'
 
-export const TimeLog = (props) => {
-  return (
-    <div className = "time-log">
-      <div className = "time-log__button">{props.isRunning ? 'STOP' : props.timeSpent}</div>
-      <div className = "time-log__description">{props.description}</div>
-      <div className = "time-log__timer"> {props.startTime} </div>
-    </div>
-    )
+export class TimeLog extends Component {
+  state = this.props
+
+  render() {
+    let spentTime = this.state.isRunning ? getSpentTime(this.state.startTime) : getSpentTime(this.state.startTime, this.state.stopTime)
+    return (
+      <div className = "time-log">
+        <div className = "time-log__button" onClick = {this.handleStartStopClick}>{this.state.isRunning ? 'STOP' : 'DONE'}</div>
+        <div className = "time-log__description">{this.state.description}</div>
+        <div className = "time-log__timer"> {spentTime} </div>
+      </div>
+    ) 
+  }
+
+  componentDidMount = ()  => {
+    if (this.state.isRunning) {
+      this.interval = setInterval(this.updateTimer.bind(this), 1000)
+    }
+  }
+
+  updateTimer = () => {
+    this.forceUpdate()
+  }
+
+  handleStartStopClick = () => {
+    if (this.state.isRunning) {
+      var newState = {...this.state, isRunning: false, stopTime: new Date()}
+      this.setState(newState)
+    }
+  }
+}
+
+TimeLog.propTypes = {
+  description: React.PropTypes.string.isRequired,
+  startTime: React.PropTypes.instanceOf(Date).isRequired,
+  id: React.PropTypes.number.isRequired
 }
--- app/components/application/time_log/time_log_form.component.js
+++ app/components/application/time_log/time_log_form.component.js
@@ -2,14 +2,23 @@ import React from 'react'
 
 export const TimeLogForm = (props) => (
   <div className = "application__new-time-log">
-    <div className = "time-log">
-      <div className = "time-log__button">PLAY</div>
+    <form className = "time-log__form"
+          onSubmit = {props.handleSubmit}>
+          
+      <div className = "time-log__button"
+           onClick = {props.handleSubmit}>PLAY</div>
       <input type = "text"
       className = "time-log__description" 
       value = {props.description}
       onChange = {props.handleInputChange}
       />
       <div className = "time-log__timer"> 88:88 </div>
-    </div>
+    </form>
   </div> 
   )
+
+TimeLogForm.propTypes = {
+  description: React.PropTypes.string.isRequired,
+  handleInputChange: React.PropTypes.func.isRequired,
+  handleSubmit: React.PropTypes.func.isRequired
+}
